75cf3517973983081bf26c66a0de8e243760eac6
Geraldo <geraldo.figueiredosj@gmail.com>
2020-11-13 01:03:07 -0300

    feat: UserValidator instance created and injected to UserControl (dependency)

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 import business.control.UserControl;
+import business.control.UserValidator;
+import business.model.User;
 import infra.UserDao;
 import infra.UserFileDao;
+import util.UserLoginException;
 import view.UserForm;
 import view.UserFormConsole;
 
 public class Main {
     public static void main(String[] args) {
         UserDao userDao = new UserFileDao();
-        UserControl controller = new UserControl(userDao);
+        UserValidator userValidator = new UserValidator();
+        UserControl controller = new UserControl(userDao, userValidator);
         UserForm userForm = new UserFormConsole(controller);
 
         userForm.showUserForm();
