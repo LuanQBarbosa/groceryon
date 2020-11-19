205150abfc219922676774b9e5e8dfe6c71a06d6
Tulio Souza <tulio.souza99@gmail.com>
2020-11-19 08:09:36 -0300

    Change ComparadorData to UserBirthdayComparator and fix compare order

--- business/model/UserBirthdayComparator.java
+++ business/model/UserBirthdayComparator.java
@@ -3,10 +3,10 @@ package business.model;
 import java.util.Comparator;
 import business.model.User;
 
-public class ComparadorData implements Comparator<User> {
+public class UserBirthdayComparator implements Comparator<User> {
 
     @Override
     public int compare(User user1, User user2){
-        return user1.getBirthday().compareTo(user2.getBirthday());
+        return user2.getBirthday().compareTo(user1.getBirthday());
     }
 }
