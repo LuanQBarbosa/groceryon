a23dfd9233983ce6306b75f286eea24e682fe45e
Luan Barbosa <luanqbarbosa@gmail.com>
2020-11-23 11:00:08 -0300

    singleton design pattern implemented on facade

--- Main.java
+++ Main.java
@@ -1,13 +1,4 @@
 import business.Facade;
-import business.control.ProductControl;
-import business.control.UserControl;
-import business.control.UserValidator;
-import business.model.Date;
-import infra.ProductDao;
-import infra.ProductFileDao;
-import infra.UserDao;
-import infra.UserFileDao;
-import util.InfraException;
 import util.InvalidOptionException;
 import util.UserLoginException;
 import view.Form;
@@ -15,21 +6,7 @@ import view.FormConsole;
 
 public class Main {
     public static void main(String[] args) throws InvalidOptionException, UserLoginException {
-        UserDao userDao = new UserFileDao("users.ser");
-        UserValidator userValidator = new UserValidator();
-        UserControl userController = null;
-
-        ProductDao productDao = new ProductFileDao("products.ser");
-        ProductControl productController = null;
-        try {
-            userController = new UserControl(userDao, userValidator);
-            productController = new ProductControl(productDao);
-        } catch(InfraException e) {
-            System.out.println("Houve um erro ao iniciar a aplicação.");
-            System.exit(1);
-        }
-
-        Facade facade = new Facade(userController, productController);
+        Facade facade = Facade.getInstance();
 
         Form form = new FormConsole(facade);
 
--- business/Facade.java
+++ business/Facade.java
@@ -5,21 +5,51 @@ import java.util.TreeSet;
 
 import business.control.ProductControl;
 import business.control.UserControl;
+import business.control.UserValidator;
 import business.model.Date;
 import business.model.User;
+import infra.ProductDao;
+import infra.ProductFileDao;
+import infra.UserDao;
+import infra.UserFileDao;
 import util.IncorrectDateFormatException;
 import util.InfraException;
 import util.InvalidOptionException;
 import util.UserLoginException;
 import util.UserPasswordException;
 
+// ** Facade Design Pattern implementation **
 public class Facade {
     private UserControl userController;
     private ProductControl productController;
+    private static Facade instance = null;
 
-    public Facade(UserControl userController, ProductControl producController) {
+    private Facade() {
+        UserDao userDao = new UserFileDao("users.ser");
+        UserValidator userValidator = new UserValidator();
+        UserControl userController = null;
+
+        ProductDao productDao = new ProductFileDao("products.ser");
+        ProductControl productController = null;
+        try {
+            userController = new UserControl(userDao, userValidator);
+            productController = new ProductControl(productDao);
+        } catch(InfraException e) {
+            System.out.println("Houve um erro ao iniciar a aplicação.");
+            System.exit(1);
+        }
+
         this.userController = userController;
-        this.productController = producController;
+        this.productController = productController;
+    }
+
+    // ** Singleton Design Pattern implementation **
+    public static Facade getInstance() {
+        if(instance == null) {
+            instance = new Facade();
+        }
+
+        return instance;
     }
 
     public void userSignUp(String login, String password, String birthday) throws IncorrectDateFormatException, UserLoginException, UserPasswordException, InfraException {
